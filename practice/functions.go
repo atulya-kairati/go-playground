7c8682253c9c90dd4982edb9c0aebbe3e1c6ffd8
atulya-kairati <atulyakairati@gmail.com>
2023-05-06 15:31:38 +0530

    functions

--- practice/functions.go
+++ practice/functions.go
@@ -12,7 +12,21 @@ func main() {
   fmt.Printf("%T\n", hello)
   greet_all(greeter, "me", "you", "others")
   
-  greet_all(greeter, "Manus", "Atulya", "Chootad")
+  greet_all(hello, "Manus", "Atulya", "Chootad")
+  
+  fmt.Println(logical_or(true, false))
+  
+  dm := get_divmod()
+  fmt.Printf("%T\n", dm)
+  div, mod := dm(10, 3)
+  fmt.Println(div, mod)
+  
+  // anon func
+  af := func() { fmt.Println("Me gusta anonymous") }
+  
+  af()
+  
+  greet_all(func(name string) { fmt.Println(name, "chutiya") },"atulya", "manus", "chootadi lal")
   
 }
 
@@ -21,8 +35,27 @@ func hello(name string){
   fmt.Println("Hello!", name)
 }
 
+
 func greet_all(greeting func(string), people ...string){
   for _, person := range people {
-    hello(person)
+    greeting(person)
   }
+}
+
+
+// function with return type
+func logical_or(a bool, b bool) bool {
+  return a || b
+}
+
+
+// function with multiple return type (they can be of different types)
+func divmod(a int, b int) (int, int) {
+  return a / b, a % b
+}
+
+
+// function with function as a return type
+func get_divmod() func(int, int) (int, int) {
+  return divmod
 }
