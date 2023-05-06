9bdb7893848716c9f594d68b415fc284ff6710c7
atulya-kairati <atulyakairati@gmail.com>
2023-05-06 11:04:49 +0530

    switch

--- practice/switch.go
+++ practice/switch.go
@@ -7,9 +7,26 @@ import (
 
 func main() {
   
-  switch  {
-  case condition:
+  n := 4
+  
+  switch(n)  {
+  case 1:
+    fmt.Println("one")
     
+  case 2, 3, 4:
+    fmt.Println("2,3,4")
+    
+  }
+  
+  switch  {
+  case n > 0:
+    fmt.Println("+")
+    
+  case n < 0:
+    fmt.Println("-")
+  
+  default:
+    fmt.Println("0")
   }
   
 }
